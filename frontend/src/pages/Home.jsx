dfdfc998dc96f1f2cbd2c64c4c4f6abe512274dc
rvbana02 <banasikku@gmail.com>
2026-04-18 23:22:42 +0530

    this is the commit message for the initial commit of the frontend part of the e-commerce application. It includes the basic structure of the React application, along with necessary assets and components to get started with the development of the frontend.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-center mt-10'>Welcome to the Home Page</h1>
+      <p className='text-center mt-5 text-gray-600'>This is the home page of our application. Explore our collection and learn more about us.</p>
+    </div>
+  )
+}
+
+export default home
